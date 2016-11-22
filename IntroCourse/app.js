dd0c57ac691dd27992215eb04df8a835f0da5ac4
PureRandom <info@purernadom.co.uk>
2016-11-22 18:07:00 +0000

    module1 attempt 1

--- IntroCourse/app.js
+++ IntroCourse/app.js
@@ -3,12 +3,12 @@
 
     angular.module('myFirstApp', [])
     // Can pass over a function/callback or an array of strings, which are the variable that wil get passed over and then the method at the end.
-        .controller('myFirstController', ['$scope', '$filter', '$injector', DIController]);
+        .controller('myFirstController', DIController);
 
     // If you don't use the array method above, you can also inject the variables like..
     DIController.$inject = ['$scope', '$filter'];
 
-    function DIController($scope, $filter, $injector) {
+    function DIController($scope, $filter) {
         $scope.name = 'Chris Pateman';
 
         $scope.upper = () => {
---
+++ module1-solution/app.js
@@ -0,0 +1,27 @@
+(function() {
+    'use strict';
+
+    angular.module('LunchCheck', [])
+        .controller('LunchCheckController', checkList);
+
+    checkList.$inject = ['$scope'];
+
+    function checkList($scope) {
+        $scope.listInput = 'bing,bong';
+        $scope.message = '';
+
+        $scope.listCheck = () => {
+            let listItems = $scope.listInput.split(',');//.filter(n => n !== undefined);
+            if (listItems.length === 0) {
+                $scope.message = 'Please enter data first';
+            } else if (listItems.length <= 3) {
+                $scope.message = 'Enjoy!';
+            } else if (listItems.length > 3) {
+                $scope.message = 'Too much!';
+            } else {
+                $scope.message = '';
+            }
+        };
+        
+    }
+})();
