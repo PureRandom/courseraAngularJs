ea43250997a7f9eedb70dd466285c0a2dfde4cc1
PureRandom <info@purernadom.co.uk>
2016-11-22 17:00:08 +0000

    DependencyInjection

--- IntroCourse/app.js
+++ IntroCourse/app.js
@@ -1,11 +1,18 @@
-(function () {
+(function() {
     'use strict';
 
     angular.module('myFirstApp', [])
+        .controller('myFirstController', DIController);
 
-        .controller('myFirstController', function ($scope) {
-            $scope.name = "Chris Pateman";
-            $scope.displayField = "ShowMe"
-        });
+    function DIController($scope, $filter, $injector) {
+        $scope.name = 'Chris Pateman';
 
+        $scope.upper = () => {
+            const upCase = $filter('uppercase');
+            $scope.name = upCase($scope.name);
+        };
+
+        // This will display all the variables passed to the method
+        console.log($injector.annotate(DIController));
+    }
 })();
