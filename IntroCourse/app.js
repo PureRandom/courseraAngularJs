f5f8703bd887e525c3771c0c8dae3096249e620c
PureRandom <info@purernadom.co.uk>
2016-11-22 17:07:10 +0000

    Prevent Minify Injection

--- IntroCourse/app.js
+++ IntroCourse/app.js
@@ -2,7 +2,11 @@
     'use strict';
 
     angular.module('myFirstApp', [])
-        .controller('myFirstController', DIController);
+    // Can pass over a function/callback or an array of strings, which are the variable that wil get passed over and then the method at the end.
+        .controller('myFirstController', ['$scope', '$filter', '$injector', DIController]);
+
+    // If you don't use the array method above, you can also inject the variables like..
+    DIController.$inject = ['$scope', '$filter'];
 
     function DIController($scope, $filter, $injector) {
         $scope.name = 'Chris Pateman';
@@ -13,6 +17,6 @@
         };
 
         // This will display all the variables passed to the method
-        console.log($injector.annotate(DIController));
+        // console.log($injector.annotate(DIController));
     }
 })();
