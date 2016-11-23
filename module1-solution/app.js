85a5099c9e3446571e8d331a9b6f41ca91e5d43c
Admin <Admin>
2016-11-23 09:02:10 +0000

    Bonus Question

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -12,15 +12,19 @@
 
         $scope.listCheck = () => {
             let listItems = $scope.listInput.split(',').filter(n => n !== undefined && n !== '');
+            let status = false;
             if (listItems.length === 0) {
                 $scope.message = 'Please enter data first';
             } else if (listItems.length <= 3) {
                 $scope.message = 'Enjoy!';
+                status = true;
             } else if (listItems.length > 3) {
                 $scope.message = 'Too much!';
+                status = true;
             } else {
                 $scope.message = '';
             }
+            status ? $scope.statusColor = 'green' : $scope.statusColor = 'red';
         };
         
     }
